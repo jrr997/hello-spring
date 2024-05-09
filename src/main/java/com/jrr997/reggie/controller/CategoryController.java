4b2d4ba2faef0c17629f5bc1f8862dba62a33454
jrr997 <997571692@qq.com>
2024-05-09 21:36:06 +0800

    feat: category查询和删除接口

--- src/main/java/com/jrr997/reggie/controller/CategoryController.java
+++ src/main/java/com/jrr997/reggie/controller/CategoryController.java
@@ -1,6 +1,16 @@
 package com.jrr997.reggie.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.jrr997.reggie.common.Result;
+import com.jrr997.reggie.entity.Category;
+import com.jrr997.reggie.entity.Employee;
+import com.jrr997.reggie.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +24,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-05-08 11:40:44
  */
 @RestController
-@RequestMapping("/reggie/category")
+@RequestMapping("/category")
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
 
+    @GetMapping("/page")
+    public Result<Page<Category>> page(int page, int pageSize){
+        Page pageInfo = new Page(page, pageSize);
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper();
+        wrapper.orderByAsc(Category::getSort);
+        categoryService.page(pageInfo, wrapper);
+        return Result.success(pageInfo);
+    }
+
+    @DeleteMapping()
+    public Result<String> page(Long id){
+        categoryService.removeById(id);
+        return Result.success("删除成功");
+    }
 }
